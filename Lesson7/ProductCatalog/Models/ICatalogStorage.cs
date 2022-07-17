35208aa4cd8d9ceaf3427a62650b2aad871ca3f5
JackenQuake <jackenquake@gmail.com>
2022-07-17 10:54:11 +0300

    Lesson7 task done

---
+++ Lesson7/ProductCatalog/Models/ICatalogStorage.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ProductCatalog.Models
+{
+	public interface ICatalogStorage
+	{
+		public int CountCategories(CancellationToken token = default);
+
+		public bool HasAnyCategories(CancellationToken token = default);
+
+		public Category GetCategory(int categoryId, CancellationToken token = default);
+
+		public IEnumerable<Category> GetAllCategories(CancellationToken token = default);
+
+		public void AddCategory(Category newData, CancellationToken token = default);
+
+		public void UpdateCategory(Category newData, CancellationToken token = default);
+
+		public void DeleteCategory(int categoryId, CancellationToken token = default);
+
+		public int CountProducts(int categoryId, CancellationToken token = default);
+
+		public bool HasAnyProducts(int categoryId, CancellationToken token = default);
+
+		public Product GetProduct(int categoryId, int productId, CancellationToken token = default);
+
+		public IEnumerable<Product> GetAllProducts(int categoryId, CancellationToken token = default);
+
+		public void AddProduct(int categoryId, Product newData, CancellationToken token = default);
+
+		public void UpdateProduct(int categoryId, Product newData, CancellationToken token = default);
+
+		public void DeleteProduct(int categoryId, int productId, CancellationToken token = default);
+	}
+}
